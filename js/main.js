c71a23f65ede35021d1ab9c74adc37ab79764168
Anna <anna1995dzhym@gmail.com>
2018-08-24 01:39:06 +0300

    Added files 2

--- js/main.js
+++ js/main.js
@@ -6,17 +6,20 @@
         slidesToShow: 3,
         slidesToScroll: 1,
         slide: 'div',
-        draggable: true,
         prevArrow: '.prev',
         nextArrow: '.next',
         dotsClass: 'slick-dots carousel__dots',
     });
-    // $(document).ready(function () {
-    //     $('.home').on('click', function () {
-    //         e.preventDefault();
-    //
-    //         $('.carousel').slickGoTo(1);
-    //     })
-    // })
+    $(document).ready(function () {
+        $('.home').on('click', function () {
+            e.preventDefault();
+
+            $('.carousel').slickGoTo(1);
+        })
+    });
+    $('.carousel').click(function next() {
+        $('.carousel').slick('slickNext');
+    })
+
 
 })(jQuery);
